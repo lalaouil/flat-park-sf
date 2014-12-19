d6bf814f8d98617e128e6a203e664c546f8ffa75
Laurie Alaoui <laurie.alaoui@gmail.com>
2014-12-19 10:14:07 -0800

    change

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ var db        = {};
 // NEEDED FOR HEROKU ///////////
 if(config.use_env_variable){
   var db_info = process.env[config.use_env_variable].match(/([^:]+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-  config.dialect=db_info[1];
+  config.dialect= "postgres"
   config.username=db_info[2];
   config.password=db_info[3];
   config.host=db_info[4];  
